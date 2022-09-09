dea6898e36bd63eb01cea8860cbb3ef3d4bbdde2
Oyinbayode <oyinbayode75@gmail.com>
2022-09-09 21:15:35 +0100

    feat: completed cards section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
+import { Card, Grid } from "@mantine/core";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import ConnectWallet from "../src/components/ConnectWallet/ConnectWallet";
+import DashbordCard from "../src/components/Dashboard/Card/Card";
 import styles from "../src/shared/styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <ConnectWallet />
+      <DashbordCard />
     </div>
   );
 }
