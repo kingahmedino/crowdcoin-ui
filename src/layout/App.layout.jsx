3532be4713e95bc461d87e8be33ee067e3ff57ec
Oyinbayode <oyinbayode75@gmail.com>
2022-09-11 22:53:09 +0100

    feat: Modified Connect Wallet Header Styles

--- src/layout/App.layout.jsx
+++ src/layout/App.layout.jsx
@@ -1,12 +1,52 @@
-import { AppShell, Navbar,Header  } from "@mantine/core";
+import { AppShell, Navbar, Header } from "@mantine/core";
 import { Nav } from "../components/Nav";
 import ConnectWallet from "../components/ConnectWallet/ConnectWallet";
 export default function AppLayout({ Component, pageProps, router }) {
+  const { pathname } = router;
+
+  const Name =
+    pathname === "/"
+      ? "Dashboard"
+      : pathname.includes("campaign")
+      ? "Campaigns"
+      : pathname.includes("withdrawal")
+      ? "Withdrawals"
+      : pathname.includes("contribution")
+      ? "My Contributions"
+      : "Dashboard";
+
   return (
     <AppShell
       navbarOffsetBreakpoint="sm"
       fixed={false}
-      header={<Header height={60} style={{display:"flex", border: "none", justifyContent: "flex-end", position:"absolute", backgroundColor: "transparent"}}   p="xs"><ConnectWallet/></Header>}
+      header={
+        <Header
+          height={136}
+          style={{
+            display: "flex",
+            border: "none",
+            justifyContent: "space-between",
+            alignItems: "center",
+            position: "absolute",
+            backgroundColor: "#f2f2f2",
+            paddingTop: "56px",
+          }}
+          p="xs"
+        >
+          <h1
+            style={{
+              marginLeft: "248px",
+              fontSize: "40px",
+              fontWeight: 500,
+              lineHeight: "44px",
+              color: "#040404",
+            }}
+          >
+            {Name}
+          </h1>
+          <ConnectWallet />
+        </Header>
+      }
       navbar={
         <Navbar width={{ base: "248px" }} height={"100vh"} p="xs">
           <Nav />
@@ -16,7 +56,7 @@ export default function AppLayout({ Component, pageProps, router }) {
         main: {
           padding: 0,
           overflowY: "scroll",
-          paddingTop: 56,
+          paddingTop: 136,
           paddingLeft: 32,
           paddingRight: 32,
           backgroundColor: "rgba(242, 242, 242, 1)",
